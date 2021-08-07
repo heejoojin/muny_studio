26c810be5db4a52d99caf8ae7b3c53da363392ae
heejoojin <hjin77@gatech.edu>
2021-08-07 18:47:46 -0400

    add popup

--- project/js/classregistration.js
+++ project/js/classregistration.js
@@ -1,38 +1,23 @@
 var count = 0;
 
 function addToCart() {
-    var msg = '<div id=\"popup-head\"><a href=""><h2 id=\"popup-close\"">&#10005;</h2></a></div>';
-    msg += '<div><h2>Google log-in is required for class registeration</h2></div>';
+    Create the HTML for the message
+    var msg = '<div class=\"header\"><a id=\"close\" href="#"><h2 id="close">&#10005;<h2></a></div>';
+    msg += '<div><h2>This Membership Webpage is Currently Under Construction</h2>';
+    msg += 'we are in the process of updating sign&ndash;up and sing&ndash;in functionality!<br/>';
+    msg += 'we recommend you to use your google account to join our membership :)<br/>thank you so much for your understanding <i class="fa fa-heart" id="heart" aria-hidden="true"></i></div>';
 
-    var login_request = document.createElement('div');       // Create a new element
-    login_request.setAttribute('class', 'popup');                // Add an id of note
-    login_request.innerHTML = msg;                           // Add the message
-    document.body.appendChild(login_request);                // Add it to the page
+    var elNote = document.createElement('div');       // Create a new element
+    elNote.setAttribute('id', 'note');                // Add an id of note
+    elNote.innerHTML = msg;                           // Add the message
+    document.body.appendChild(elNote);                // Add it to the page
 
-    function closePopUp() {                          // Declare function
-      document.body.removeChild(login_request);              // Remove the note
+    function dismissNote() {                          // Declare function
+      document.body.removeChild(elNote);              // Remove the note
     }
 
-    var close_request = document.getElementById('popup-close');   // Get the close button
-    close_request.addEventListener('click', closePopUp, false);// Click close-clear note
-    // if (!firebase.auth().currentUser) {
-    //     var msg = '<div id=\"popup-head\"><a href=""><h2 id=\"popup-close\"">&#10005;</h2></a></div>';
-    //     msg += '<div><h2>Google log-in is required for class registeration</h2></div>';
-
-    //     var login_request = document.createElement('div');       // Create a new element
-    //     login_request.setAttribute('class', 'popup');                // Add an id of note
-    //     login_request.innerHTML = msg;                           // Add the message
-    //     document.body.appendChild(login_request);                // Add it to the page
-
-    //     function closePopUp() {                          // Declare function
-    //       document.body.removeChild(login_request);              // Remove the note
-    //     }
-
-    //     var close_request = document.getElementById('popup-close');   // Get the close button
-    //     close_request.addEventListener('click', closePopUp, false);// Click close-clear note
-    // }
-    // if user logged in - update cart
-    // if user not logged in - force the user to log in
+    var elClose = document.getElementById('close');   // Get the close button
+    elClose.addEventListener('click', dismissNote, false);// Click close-clear note
 }
 
 // $(document).ready(function() {
@@ -41,6 +26,9 @@ function addToCart() {
 //         $('#cart-count').html('counter'); 
 //     })
 // });
+
+// if user logged in - update cart
+// if user not logged in - force the user to log in
 
 
 // Create the HTML for the message
@@ -61,16 +49,17 @@ function addToCart() {
 // var elClose = document.getElementById('close');   // Get the close button
 // elClose.addEventListener('click', dismissNote, false);// Click close-clear note
 
-// $(".box1-2").hide();
-// $('button').on('click', function () {
-//     $(".box1-2").show();
-//     var classname = $(this).parent('#classdesc').find("#classname").eq(0);
-//     var nameclone = classname.clone()
-//         .css({
-//             'font-size':'15px',
-//             'display': 'block',
-//             'text-align': 'center',
-//             'margin': '10px',
-//         }).appendTo($('#registeration'));
-//     /*$("#registerclass").append('<br/>' + nameclone.text());*/
-// });
+
+$(".box1-2").hide();
+$('button').on('click', function () {
+    $(".box1-2").show();
+    var classname = $(this).parent('#classdesc').find("#classname").eq(0);
+    var nameclone = classname.clone()
+        .css({
+            'font-size':'15px',
+            'display': 'block',
+            'text-align': 'center',
+            'margin': '10px',
+        }).appendTo($('#registeration'));
+    /*$("#registerclass").append('<br/>' + nameclone.text());*/
+});
