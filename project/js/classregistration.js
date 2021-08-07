44468ced85c29e946499d823385c9693400f587e
heejoojin <hjin77@gatech.edu>
2021-08-07 18:42:47 -0400

    Update classregistration.js

--- project/js/classregistration.js
+++ project/js/classregistration.js
@@ -48,17 +48,16 @@ function addToCart() {
 // var elClose = document.getElementById('close');   // Get the close button
 // elClose.addEventListener('click', dismissNote, false);// Click close-clear note
 
-
-$(".box1-2").hide();
-$('button').on('click', function () {
-    $(".box1-2").show();
-    var classname = $(this).parent('#classdesc').find("#classname").eq(0);
-    var nameclone = classname.clone()
-        .css({
-            'font-size':'15px',
-            'display': 'block',
-            'text-align': 'center',
-            'margin': '10px',
-        }).appendTo($('#registeration'));
-    /*$("#registerclass").append('<br/>' + nameclone.text());*/
-});
+// $(".box1-2").hide();
+// $('button').on('click', function () {
+//     $(".box1-2").show();
+//     var classname = $(this).parent('#classdesc').find("#classname").eq(0);
+//     var nameclone = classname.clone()
+//         .css({
+//             'font-size':'15px',
+//             'display': 'block',
+//             'text-align': 'center',
+//             'margin': '10px',
+//         }).appendTo($('#registeration'));
+//     /*$("#registerclass").append('<br/>' + nameclone.text());*/
+// });
