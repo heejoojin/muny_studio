1aa3a778c4709baf2c2aa61df44068a6a6854e42
heejoojin <hjin77@gatech.edu>
2021-08-14 19:49:05 -0400

    Update cart page

--- project/js/cart.js
+++ project/js/cart.js
@@ -2,7 +2,18 @@
 window.onload = function() {
     if (firebase.auth().currentUser) {
         var user = firebase.auth().currentUser;
+        var userdb = firebase.database().ref('user/' + user.displayName);
         $('#welcome').html('welcome ' + user.displayName + ' !');
+
+        userdb.on('value', (snapshot)=> {
+            var userval = snapshot.val();
+            for (let i = 1; i <= 3; i++) {
+                for (let j = 1; i <= userval[i]; j++) {
+                    // var msg = '<% include ../helpers/product_in_cart %> ';
+                    $('.product').html('<% include ../helpers/product_in_cart %>');
+                }
+            }
+        });
     }
-    // var msg = '<% include ../helpers/product_in_cart %> ';
+    
 };
