e00d922fa12a7be3ccd6fdef07d327a68cf9c199
heejoojin <hjin77@gatech.edu>
2021-08-14 20:14:31 -0400

    Debug

--- project/js/cart.js
+++ project/js/cart.js
@@ -1,22 +1,22 @@
 
-window.onload = function() {
-    if (firebase.auth().currentUser) {
-        console.log('google user found!');
-        // var user = firebase.auth().currentUser;
-        // var userdb = firebase.database().ref('user/' + user.displayName);
-        // $('#welcome').html('welcome ' + user.displayName + ' !');
+// window.onload = function() {
+//     if (firebase.auth().currentUser) {
+//         console.log('google user found!');
+//         // var user = firebase.auth().currentUser;
+//         // var userdb = firebase.database().ref('user/' + user.displayName);
+//         // $('#welcome').html('welcome ' + user.displayName + ' !');
 
-        // userdb.on('value', (snapshot)=> {
-        //     var userval = snapshot.val();
-        //     console.log(userval);
-        //     for (let i = 1; i <= 3; i++) {
-        //         for (let j = 1; j <= userval[i]; j++) {
-        //             // var msg = '<% include ../helpers/product_in_cart %> ';
-        //             $('.product').html('<% include ../helpers/product_in_cart %>');
-        //         }
-        //     }
-        // });
-    } else {
-        console.log('google user NOT found!');
-    }
-};
+//         // userdb.on('value', (snapshot)=> {
+//         //     var userval = snapshot.val();
+//         //     console.log(userval);
+//         //     for (let i = 1; i <= 3; i++) {
+//         //         for (let j = 1; j <= userval[i]; j++) {
+//         //             // var msg = '<% include ../helpers/product_in_cart %> ';
+//         //             $('.product').html('<% include ../helpers/product_in_cart %>');
+//         //         }
+//         //     }
+//         // });
+//     } else {
+//         console.log('google user NOT found!');
+//     }
+// };
