b8c0240684359e81f2bbbd271b8d12afcff83a6b
heejoojin <hjin77@gatech.edu>
2021-08-15 19:36:39 -0400

    Add paypal button

---
+++ project/js/pay.js
@@ -0,0 +1,10 @@
+paypal.Buttons({
+    style: {
+        layout: 'vertical',
+        color:  'black',
+        shape:  'pill',
+        label:  'paypal',
+        tagline: 'false',
+        size: 'medium'
+    }
+}).render('#paypal-button');
